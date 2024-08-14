a64a78d167d026b3b3248213cd80afd693b8fbcb
Vivek Mishra <vivekmishra172003@gmail.com>
2024-08-15 00:13:45 +0530

    database connection in adnroid done and post done now read anad is reamin

---
+++ Firebase_database/app/src/main/java/com/example/firebase_database/MainActivity.kt
@@ -0,0 +1,49 @@
+package com.example.firebase_database
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.firebase_database.databinding.ActivityMainBinding
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    lateinit var database: DatabaseReference
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        val signButton = binding.btnSignUp
+        val etName = binding.etName
+        val etMail = binding.etEmail
+        val userId = binding.etUniqueId
+        val userPassword = binding.etPassword
+
+        signButton.setOnClickListener {
+
+            val name = etName.text.toString()
+            val mail = etMail.text.toString()
+            val uniqueId = userId.text.toString()
+            val password = userPassword.text.toString()
+
+            val user = User(name, mail, password, uniqueId)
+            database = FirebaseDatabase.getInstance().getReference("Users")
+
+            database.child(uniqueId).setValue(user).addOnSuccessListener {
+                etName.text?.clear()
+                Toast.makeText(this, "User Registered", Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener {
+                Toast.makeText(this, "Failed", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    }
+}
---
+++ Firebase_database/app/src/main/java/com/example/firebase_database/User.kt
@@ -0,0 +1,5 @@
+package com.example.firebase_database
+
+data class User(val name : String, val email : String, val password : String, val uniqueId: String){
+
+}
